4b3bad241cec8ee4ed2865678d3ce3df4baef877
Nadee95 <nadeekacsuop@gmail.com>
2018-09-14 00:29:57 +0530

    fc

---
+++ routes/parts.route.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var part = require('../models/part.model');
+
+/* GET users listing. */
+router.get('/add', function(req, res, next) {
+  
+});
+
+module.exports = router;
