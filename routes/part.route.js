4a800bcf57590651ac882d3294828d0487e900d8
Nadee95 <nadeekacsuop@gmail.com>
2018-09-14 16:45:27 +0530

    build 2

---
+++ routes/part.route.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var Part = require('../models/part.model');
+
+/* GET users listing. */
+router.post('/add', function(req, res, next) {
+    var part = new Part(req.body);
+    part.save(function (err) {
+        if (err){
+          return res.json({status:500,message:"ERROR_CREATE",data:err.message});
+        } 
+        return res.json({status:200,message:"SUCCESSFUL"});
+      });
+})
+module.exports = router;
